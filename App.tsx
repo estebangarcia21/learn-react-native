259f8151c3037e0e1c6bbf06f8d6e5805b8933fb
Esteban Garcia <estebangarcia2121@gmail.com>
2021-09-24 12:32:23 -0700

    Added prettierrc

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>Hey club!</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => setCount(count + 1)}
+      >
+        <Text style={styles.buttonText}>Press Here: {count}</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +22,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10
   },
+  text: {
+    fontSize: 34,
+    fontWeight: "600",
+    marginBottom: 20
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#007AFF",
+    paddingVertical: 10,
+    width: "100%",
+    borderRadius: 2
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "500"
+  }
 });
